a5e38877c82e2ee7b53bfcc4359c3aa433e63378
kalenbet <rsmoney@rambler.ru>
2023-01-27 23:51:04 +0300

    1st_sharpcode4

---
+++ task003/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+if (b == 0) {Console.WriteLine("да");}
+else {Console.WriteLine("нет");}
+
