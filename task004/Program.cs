d0e154ef0e661f1dbe5c332b0c926446971fc0bb
kalenbet <rsmoney@rambler.ru>
2023-01-27 23:59:46 +0300

    1st_sharpcode5

---
+++ task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= a; i++)
+{ 
+    int b = i % 2;
+    if (b == 0) 
+    {
+        if (i != 2) {Console.Write(", ");}
+        Console.Write(i);
+    }
+}
