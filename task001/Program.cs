886bd1c81a533d1b379b7b12e933ba3a561c01af
kalenbet <rsmoney@rambler.ru>
2023-01-27 23:28:01 +0300

    1st_sharpcode

---
+++ task001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+Console.WriteLine("Ho!!!");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ho!!!" + b);
+if (a > b) {Console.WriteLine("max = " + a);}
+else {Console.WriteLine("max = " + b);}
