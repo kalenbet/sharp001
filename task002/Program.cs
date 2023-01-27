72d4cb6ecf5eb541ed6816dfd752fbfd32a7cad0
kalenbet <rsmoney@rambler.ru>
2023-01-27 23:39:19 +0300

    1st_sharpcode3

---
+++ task002/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if ((a >= b) && (a >= c)) {Console.WriteLine("max = " + a);}
+else {
+    if (b > c) {Console.WriteLine("max = " + b);}
+    else {Console.WriteLine("max = " + c);}
+}
