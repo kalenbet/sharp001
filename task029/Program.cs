be3163ed7950a4f849a7e691cc751fbe11485a61
kalenbet <rsmoney@rambler.ru>
2023-02-07 20:08:49 +0300

    4th_sharpcode1

---
+++ task029/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] a = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("[");
+for (int i = 0; i < 8; i++)
+{
+    Console.Write(a[i]);
+    if (i != 7)
+    {
+        Console.Write(", ");
+    }
+}
+Console.Write("]");
