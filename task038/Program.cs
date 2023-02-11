0f885dca60ba8823d6629dc63e2e50636fb64121
kalenbet <rsmoney@rambler.ru>
2023-02-11 22:43:30 +0300

    5th_sharpcode1

---
+++ task038/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int[] GenerateArray(int len, int minV, int maxV)
+{
+    int [] arr = new int[len];
+    Random a = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = a.Next(minV, maxV + 1);
+    }
+    return arr;
+}
+int n = 10;
+int min = 1000;
+int max = -1000;
+int[] mass = GenerateArray(n, -99, 99);
+Console.Write("[");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(mass[i]);
+    if (mass[i] > max)
+    {
+        max = mass[i];
+    }
+        if (mass[i] < min)
+    {
+        min = mass[i];
+    }
+    if (i != n - 1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.Write("] -> ");
+Console.Write(max - min);
