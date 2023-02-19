4fcf976da8603babf1d3b33808b7981f4776a09b
kalenbet <rsmoney@rambler.ru>
2023-02-19 20:06:20 +0300

    7th_sharpcode1

---
+++ task050/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int [,] GenerateArray(int m, int n, int min, int max)
+{
+    int [,] arr = new int[m,n];
+    Random a = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i,j] = a.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+int m = 4;
+int n = 5;
+int [,] mass = GenerateArray(m, n, 0, 20);
+Console.WriteLine("Введите номер позиции от 1 до " + (m*n));
+int p = Convert.ToInt32(Console.ReadLine());
+if (p > m*n)
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine(mass[(p - 1) / n, (p - 1) % n]);
+}
