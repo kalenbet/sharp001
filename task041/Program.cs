7d1b0e9e2710436ceded68c11816d13f5c537723
kalenbet <rsmoney@rambler.ru>
2023-02-19 15:45:33 +0300

    6th_sharpcode1

---
+++ task041/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int InputNumber(int n)
+{
+        Console.WriteLine("Введите чмсло номер "+ n);
+        return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = 5;
+int s = 0;
+for (int i = 1; i <= m; i++)
+{
+    if (InputNumber(i) > 0){s++;}
+}
+Console.WriteLine(s);
