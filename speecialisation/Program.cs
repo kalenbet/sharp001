148c5dd2cb0b1832bf66caf3b8e1b73cf78d8298
kalenbet <rsmoney@rambler.ru>
2023-03-20 00:09:58 +0300

    sharpcode1

---
+++ speecialisation/Program.cs
@@ -0,0 +1,17 @@
+﻿//Оставляем в массиве элементы менее 3 символов
+string [] arr = {"hello","2","world",":-)"};
+string [] arrRes = {};
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        Array.Resize(ref arrRes, arrRes.Length+1);
+        arrRes[arrRes.Length-1] = arr[i];
+    }
+}
+
+for (int j = 0; j < arrRes.Length; j++)
+{
+    Console.WriteLine(arrRes[j]);
+}
